19be54e1697cbcc6ced99c2798479d13647dfa2a
User <user@hackerrank.workspace>
2024-07-17 06:19:01 +0000

    Add initial repository
    Commit by HackerRank

---
+++ src/main/java/com/hackerrank/stocktrade/model/Trade.java
@@ -0,0 +1,82 @@
+package com.hackerrank.stocktrade.model;
+
+import java.sql.Timestamp;
+
+public class Trade {
+    private Long id;
+    private String type;
+    private User user;
+    private String symbol;
+    private Integer shares;
+    private Float price;
+    private Timestamp timestamp;
+    
+    public Trade() {
+    }
+    
+    public Trade(Long id, String type, User user, String symbol, Integer quantity, Float price, Timestamp timestamp) {
+        this.id = id;
+        this.type = type;
+        this.user = user;
+        this.symbol = symbol;
+        this.shares = quantity;
+        this.price = price;
+        this.timestamp = timestamp;
+    }
+    
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getType() {
+        return this.type;
+    }
+    
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    public User getUser() {
+        return this.user;
+    }
+    
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    public String getSymbol() {
+        return this.symbol;
+    }
+    
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    
+    public Integer getShares() {
+        return this.shares;
+    }
+    
+    public void setShares(Integer shares) {
+        this.shares = shares;
+    }
+    
+    public Float getPrice() {
+        return this.price;
+    }
+    
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+    
+    public Timestamp getTimestamp() {
+        return this.timestamp;
+    }
+    
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
